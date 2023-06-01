cc0296ae2ec5b1e36c9546e96cd6c73247d82f8c
baariq <baariq.fairuuz@dataon.com>
2023-06-01 11:59:37 +0700

    feature: add tealium

---
+++ tealium-script.js
@@ -0,0 +1,20 @@
+var utag_data = {
+  page_name: "home",
+  page_section: "",
+  page_subsection: "",
+  core_language: "jp",
+  core_target_country: "jp",
+  visitor_azid: "kjvw425",
+};
+
+(function (a, b, c, d) {
+  a = "//tags.tiqcdn.com/utag/astrazeneca/jp-c3quicksearch/dev/utag.js";
+  b = document;
+  c = "script";
+  d = b.createElement(c);
+  d.src = a;
+  d.type = "text/java" + c;
+  d.async = true;
+  a = b.getElementsByTagName(c)[0];
+  a.parentNode.insertBefore(d, a);
+})();
